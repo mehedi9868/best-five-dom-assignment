ae66bb26a72a1d75236e78cbc85fcd7cb4bdbdb3
mehedi9868 <mehedi9868@gmail.com>
2022-08-22 08:21:05 +0600

    added one common function

--- js/budget.js
+++ js/budget.js
@@ -1,10 +1,8 @@
 let selectedPlayerExpences = 0;
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const perPlayerExpence = document.getElementById('per-player-input');
-    const inputFieldStringValue = perPlayerExpence.value;
-    const inputFieldValue = parseFloat(inputFieldStringValue);
+    const perPlayerExpence = getInputFieldValueById('per-player-input'); // used function from common.js
     const selectedPlayerNumber = document.getElementById("selected-players").children.length
-    selectedPlayerExpences = inputFieldValue * selectedPlayerNumber;
+    selectedPlayerExpences = perPlayerExpence * selectedPlayerNumber;
     const selectedTextElement = document.getElementById('player-expences');
     selectedTextElement.innerText = selectedPlayerExpences;
     return selectedPlayerExpences;
---
+++ js/common.js
@@ -0,0 +1,8 @@
+// at least one common function for bonus.
+
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
